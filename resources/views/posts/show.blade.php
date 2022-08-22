630ee6ca796dfdc417dcf1f1a2b40ddf764dc108
QuyetVVJP <quyetvv12795@gmail.com>
2022-08-22 16:17:21 +0900

    Loops in Templates

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,6 +3,23 @@
 @section('title', $post['title'])
 
 @section('content')
+@if ($post['is_new'])
+    <h1>A New Blog</h1>
+@elseif (!$post['is_new'])
+    <div>Old Blog</div>
+@endif
+
+@unless ($post['is_new'])
+    <div>
+        It is old blog..... using unless
+    </div>
+@endunless
+
     <h1>{{ $post['title']}}</h1>
     <p>{{$post['content']}} </p>
+
+@isset($post['has_comment'])
+    <div>The blog has comment</div>
+@endisset
+
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -13,28 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home.index', []);
-})->name('home.index');
+// Route::get('/', function () {
+//     return view('home.index', []);
+// })->name('home.index');
 
 Route::get('/contact', function(){
     return view('home.contact');
 })->name('home.contact');
 
-Route::get('/posts/{id?}', function($id){
-    $posts = [
-        1 => [
-            'title' => 'Intro to Laravel',
-            'content' => 'This is a short intro to Laravel'
-        ],
-        2 => [
-            'title' => 'Intro to PHP',
-            'content' => 'This is a short intro to PHP'
-        ]
-    ];
+$posts = [
+    1 => [
+        'title' => 'Intro to Laravel',
+        'content' => 'This is a short intro to Laravel',
+        'is_new' => true,
+        'has_comment' => true
+    ],
+    2 => [
+        'title' => 'Intro to PHP',
+        'content' => 'This is a short intro to PHP',
+        'is_new' => false
+    ]
+];
+
+Route::get('/posts', function() use($posts){
+    return view('posts.index', ['posts' => $posts]);
+});
+
+Route::get('/posts/{id?}', function($id) use($posts){
 
     abort_if(!isset($posts[$id]), 404);
     return view('posts.show', ['post' =>$posts[$id]]);
 });
+
+Route::view('/', 'home.index')->name('home.index');
 
 
