be8fd07a45935e00f41f45aecd2091b8bb3c1b3d
QuyetVVJP <quyetvv12795@gmail.com>
2022-08-22 16:29:40 +0900

    partial

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,9 +3,10 @@
 @section('title', 'Blog Posts')
 
 @section('content')
-    @foreach ($posts as $post )
-        <div>{{$post['title'] }}</div>
-
-    @endforeach
+@forelse ($posts as $post )
+    @include('posts.partials.post')
+@empty
+    No post found!
+@endforelse
 
 @endsection
