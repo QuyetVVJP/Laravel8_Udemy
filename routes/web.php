c2849d4e86b0b2d4b75049eb1f8d235592be0c9e
QuyetVVJP <quyetvv12795@gmail.com>
2022-08-22 15:53:16 +0900

    Passing and Rendering Data in Templates

--- routes/web.php
+++ routes/web.php
@@ -21,8 +21,20 @@ Route::get('/contact', function(){
     return view('home.contact');
 })->name('home.contact');
 
-Route::get('/posts/{id?}', function($id = 0){
-    return 'Blog '.$id;
+Route::get('/posts/{id?}', function($id){
+    $posts = [
+        1 => [
+            'title' => 'Intro to Laravel',
+            'content' => 'This is a short intro to Laravel'
+        ],
+        2 => [
+            'title' => 'Intro to PHP',
+            'content' => 'This is a short intro to PHP'
+        ]
+    ];
+
+    abort_if(!isset($posts[$id]), 404);
+    return view('posts.show', ['post' =>$posts[$id]]);
 });
 
 
