9f94d1bf265feccbe9d6f168c2d251532dd3efc4
QuyetVVJP <quyetvv12795@gmail.com>
2022-08-24 15:01:38 +0900

    CRUD

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -84,7 +84,7 @@ class PostController extends Controller
      */
     public function edit($id)
     {
-        //
+        return view('posts.edit', ['post' => BlogPost::findOrfail($id)]);
     }
 
     /**
@@ -94,9 +94,16 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(StorePost $request, $id)
     {
-        //
+        $post = BlogPost::findOrFail($id);
+        $validated = $request->validated();
+        $post->fill($validated);
+        $post->save();
+
+        $request->session()->flash('status', 'Blog was updated!');
+
+        return redirect(route('posts.show', ['post' => $post->id]));
     }
 
     /**
@@ -107,6 +114,10 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = BlogPost::findOrFail($id);
+        $post->delete();
+
+        session()->flash('status', 'Blog post was deleted!');
+        return redirect()->route('posts.index');
     }
 }
--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,1 +1,9 @@
 <div>{{$post['title'] }}</div>
+
+<div>
+    <form action="{{route('posts.destroy', ['post' => $post->id])}}" method="POST">
+        @csrf
+        @method('DELETE')
+        <input type="submit" value="Delete">
+    </form>
+</div>
